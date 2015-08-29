88939110c6eb1a96bddc421eeb4e0c0ff9faf05e
billzichos <billzichos@hotmail.com>
2015-08-29 09:37:36 -0400

    commit gather script

--- gather_data.R
+++ gather_data.R
@@ -20,27 +20,9 @@ downloadKaggle("titanic","gendermodel.py")
 downloadKaggle("titanic","genderclassmodel.py")
 downloadKaggle("titanic","myfirstforest.py")
 
-
-trnFile <- "train.csv"
-tstFile <- "test.csv"
-
-download.file(trnUrl, paste(wd,"\\train.csv", sep=""), quiet=TRUE)
-download.file(tstUrl, paste(wd,"\\test.csv", sep=""), quiet=TRUE)
-
 datalist <- list(
         train=read.csv(
                 paste(wd,"\\",trnFile, sep=""), header=TRUE, as.is=TRUE), 
         test=read.csv(
                 paste(wd,"\\",tstFile, sep=""), header=TRUE, as.is=TRUE)
         )
-
-downloadKaggle <- function(competitionName, fileName) {
-        kaggleUrl <- "https://www.kaggle.com/c/"
-        download.file(
-                paste(
-                        kaggleUrl,competitionName,'/download/',fileName,sep=""
-                ),
-                paste(getwd(),"\\",fileName,sep=""),
-                quiet = FALSE
-        )
-}
