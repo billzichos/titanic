7fa1a967734e6646d075c2b33a27915a0f9929a3
billzichos <billzichos@hotmail.com>
2015-09-11 16:10:14 -0400

    process data

--- process_data.R
+++ process_data.R
@@ -24,5 +24,51 @@ df$Sex <- as.factor(df$Sex, ordered = FALSE)
 df$Fare[which(is.na(df$Fare))] <- median(df$Fare, na.rm=TRUE)
 
 # impute Embarked by marking all NULLS as originating in Southampton
-fulldatalist$Embarked[which(fulldatalist$Embarked=="")] <- "S"
-fulldatalist$Embarked <- as.factor(fulldatalist$Embarked)
+df$Embarked[which(df$Embarked=="")] <- "S"
+df$Embarked <- as.factor(df$Embarked)
+
+# Name
+library(reshape)
+nameSplit <- colsplit(df$Name, "[,.] | [ ]", c("lastName", "title", "otherName1", "otherName2"))
+df <- cbind(df, nameSplit)
+df$lastName <- as.character(df$lastName)
+df$otherName1 <- as.character(df$otherName1)
+df$otherName2 <- as.character(df$otherName2)
+df$titleRollup1 <-
+        as.factor(ifelse(df$title=="Mr", "MR",
+                         ifelse(df$title=="Master", "MR",
+                                ifelse(df$title=="Don", "MR",
+                                       ifelse(df$title=="Dona", "MISS",
+                                              ifelse(df$title=="Miss", "MISS",
+                                                     ifelse(df$title=="Ms", "MISS",
+                                                            ifelse(df$title=="Mlle", "MISS",
+                                                                   ifelse(df$title=="Mrs", "MRS",
+                                                                          ifelse(df$title=="Mme", "MRS",
+                                                                                 ifelse(df$title=="Rev", "REV",
+                                                                                        ifelse(df$title=="Dr", "DR",
+                                                                                               ifelse(df$title=="the Countess", "COUNTESS",
+                                                                                                      ifelse(df$title=="Major", "MAJ",
+                                                                                                             ifelse(df$title=="Lady", "LADY",
+                                                                                                                    ifelse(df$title=="Sir", "SIR",
+                                                                                                                           ifelse(df$title=="Col", "COL",
+                                                                                                                                  ifelse(df$title=="Capt", "CAPT",
+                                                                                                                                         ifelse(df$title=="Jonkheer", "JONKHEER", "Unknown")))))))))))))))))))
+df$titleRollup2 <-
+        as.factor(ifelse(df$title=="Mr", "OTHER",
+                         ifelse(df$title=="Master", "OTHER",
+                                ifelse(df$title=="Don", "ROYALTY",
+                                       ifelse(df$title=="Dona", "ROYALTY",
+                                              ifelse(df$title=="Miss", "OTHER",
+                                                     ifelse(df$title=="Ms", "OTHER",
+                                                            ifelse(df$title=="Mlle", "OTHER",
+                                                                   ifelse(df$title=="Mrs", "OTHER",
+                                                                          ifelse(df$title=="Mme", "OTHER",
+                                                                                 ifelse(df$title=="Rev", "CLERGY",
+                                                                                        ifelse(df$title=="Dr", "DOCTOR",
+                                                                                               ifelse(df$title=="the Countess", "ROYALTY",
+                                                                                                      ifelse(df$title=="Major", "MILITARY",
+                                                                                                             ifelse(df$title=="Lady", "ROYALTY",
+                                                                                                                    ifelse(df$title=="Sir", "ROYALTY",
+                                                                                                                           ifelse(df$title=="Col", "MILITARY",
+                                                                                                                                  ifelse(df$title=="Capt", "MILITARY",
+                                                                                                                                         ifelse(df$title=="Jonkheer", "ROYALTY", "Unknown")))))))))))))))))))
